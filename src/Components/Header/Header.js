12ab7a545e240b0fbd243d12af82ab71d528e138
unknown <irfanulislamfahim@gmail.com>
2022-04-28 01:22:04 +0600

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,43 +1,33 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div className='sticky-top'>
-            {/* <Link to='/'>Home</Link>
-            <Link to='/reviews'>Reviews</Link>
-            <Link to='/dashboard'>Dashboard</Link>
-            <Link to='/blogs'>Blogs</Link>
-            <Link to='/about'>About</Link> */}
-            <hr style={{ color: 'blue', padding: '5px', margin: '0' }} />
-            <Navbar bg="dark" variant="dark" expand="lg" >
-                <Container fluid>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Navbar.Brand href="/" ><span className='brandLink'>Laptop </span>Analysis </Navbar.Brand>
-                        <Nav
-                            className="ms-auto my-2 my-lg-0 navLink"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <Link className='navLink' to="/">Home</Link>
-                            <Link className='navLink' to='/reviews'>Reviews</Link>
-                            <Link className='navLink' to='/dashboard'>Dashboard</Link>
-                            <Link className='navLink' to='/blogs'>Blogs</Link>
 
-                            <hr />
+        <Navbar sticky='top' bg="dark" variant="dark" expand="lg" >
+            <Container fluid>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Navbar.Brand href="/" ><span className='brandLink'>Laptop </span>Analysis </Navbar.Brand>
+                    <Nav
+                        className="ms-auto my-2 my-lg-0 navLink"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Link className='navLink' to="/">Home</Link>
+                        <Link className='navLink' to='/reviews'>Reviews</Link>
+                        <Link className='navLink' to='/dashboard'>Dashboard</Link>
+                        <Link className='navLink' to='/blogs'>Blogs</Link>
+                    </Nav>
 
-                        </Nav>
+                </Navbar.Collapse>
 
-                    </Navbar.Collapse>
+            </Container>
 
-                </Container>
+        </Navbar>
 
-            </Navbar>
-            <hr style={{ color: 'blue', padding: '5px', margin: '0' }} />
-        </div >
     );
 };
 
